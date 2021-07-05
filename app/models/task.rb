a7e3599fb31442a7c65bcfeef35cdbd6b8fcaac5
moro0823 <basell.4rm.0823@outlook.com>
2021-07-05 12:33:07 +0900

    [Add] csv

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,6 +15,26 @@ class Task < ApplicationRecord
   def self.ransack_associatinos(auth_object = nil)
     []
   end
+
+  #CSVデータにどの属性をどの順番で出力するかを定義
+  def self.csv_attributes
+    ["name","description", "created_at","updated_at"]
+  end
+
+  def self.generate_csv
+    #CSVデータの文字列を生成（生成した文字列がgenerate_csvクラスメソッドの戻り値になる
+    CSV.generate(headers: true) do |csv|
+      #CSVの１行目としてヘッダを出力する
+      csv << csv_attributes
+      #全タスクを取得し、１レコードごとにCSVの１行を出力する
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr)}
+        #mapメソッドでcsv_attributes配列の最初の要素をブロック（{}内）のattr変数に代入
+        #代入されたattr変数の値でtask.send(attr)でtaskに結びついたattr変数を取得
+        #新たな配列をcsvに出力
+      end
+    end
+  end
   
   private
   #nameカラムに,を入れないバリデーション
